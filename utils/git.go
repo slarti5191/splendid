9a97e6c1ed6a3aff02ac7a6b3d7a9c7fbcaafb91
Dustin Graham <dusting@aja.com>
2017-09-11 00:15:27 -0700

    SSH proof of concept.

--- utils/git.go
+++ utils/git.go
@@ -1,1 +1,1 @@
-package splendid
+package utils
--- utils/ssh.go
+++ utils/ssh.go
@@ -1,1 +1,37 @@
-package splendid
+package utils
+
+import (
+	"golang.org/x/crypto/ssh"
+	"log"
+	"fmt"
+	"net"
+)
+
+func Test() {
+	config := &ssh.ClientConfig{
+		User: "user",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("pass"),
+		},
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	}
+	conn, err := ssh.Dial("tcp", "localhost:22", config)
+	if err != nil {
+		log.Fatalf("Failed to dial: %s", err)
+	}
+	defer conn.Close()
+
+	sess, err := conn.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create session: %s", err)
+	}
+	// Example file for cisco.
+	out, err := sess.CombinedOutput("cat /conf/config.xml")
+	if err != nil {
+		panic(err)
+	}
+	// Currently just dumps to command line, but can save string(out) to file.
+	fmt.Println(string(out))
+}
