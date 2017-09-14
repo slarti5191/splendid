867bd81230e7909c2548f24a757787622822f863
sigit <sigit@aja.com>
2017-09-13 19:00:20 -0700

    Rework command assignment

--- collectors/cisco.go
+++ collectors/cisco.go
@@ -2,7 +2,7 @@ package collectors
 
 // devCisco lowercase means this is private to the collectors package.
 type devCisco struct {
-	commands map[string]string
+	commands []map[string]string
 }
 
 // Collect method is all that is necessary to implement the interface.
@@ -10,14 +10,18 @@ func (c devCisco) Collect() string {
 	return "<xml>Example</xml>"
 }
 
-// MakeCisco creates a struct that implements the Collector
+// makeCisco creates a struct that implements the Collector
 // interface for collecting Cisco configs.
 func makeCisco() Collector {
 	// Set commands to their expected output (last line)
 	return devCisco{
-		map[string]string{
-			"set pager": "",
-			"show run":  "#",
+		[]map[string]string{
+			{
+				"set pager": "",
+			},
+			{
+				"show run": "#",
+			},
 		},
 	}
 }
